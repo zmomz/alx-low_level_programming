60b891066d3a5c5342ebe8027d3f987a996b47fe
zmomz <zmzeromz@gmail.com>
2022-12-09 15:34:05 +0200

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%i",i);
+	putchar('\n');
+
+	return (0);
+}
