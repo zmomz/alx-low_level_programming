138affe0dfd332763398135c4e524cc7ea82bafe
zmomz <zmzeromz@gmail.com>
2022-12-09 17:16:04 +0200

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - putchar alphabets
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int l;
+	for (i = 0; i <= 9; i++)
+	{
+		for(l = 0; l <= 9; l++)
+		{
+			if (i < l)
+			{
+				putchar((i % 10) + '0');
+				putchar((l % 10) + '0');
+				
+				if (i != 8 || l != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
