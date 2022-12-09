858451c45b6ef1d3c9013f13ae5350be7911f32d
zmomz <zmzeromz@gmail.com>
2022-12-09 15:25:52 +0200

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - putchar alphabets
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q' && letter != 'e')
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
