849f74a881cae6a84cfd33a9b880c7c3bee772c2
zmomz <zmzeromz@gmail.com>
2022-12-09 21:07:22 +0200

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int i;
 	int l;
-	for (i = 0; i <= 9; i++)
+
+	for(i = 0; i <= 9; i++)
 	{
 		for(l = 0; l <= 9; l++)
 		{
-			if (i < l)
+			if(i < l)
 			{
 				putchar((i % 10) + '0');
 				putchar((l % 10) + '0');
