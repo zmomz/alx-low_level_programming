eab4cb0a8e25c269760032edf2cfb2f3c1212923
zmomz <zmzeromz@gmail.com>
2022-12-13 14:42:59 +0200

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - estimates the absolute value
+ * @n: input to be estimates
+ * Return: The absolute value of the input
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
