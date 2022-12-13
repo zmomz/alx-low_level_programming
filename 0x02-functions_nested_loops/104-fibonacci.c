3d3d0f0ec5dcbda2e455edac8ca5a4bee9db32a6
zmomz <zmzeromz@gmail.com>
2022-12-13 23:49:10 +0200

    cheated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,63 @@
 #include <stdio.h>
 
+
 /**
- * main - print fibs from 0 to 100
- * Return: Always 0
+ * numLength - returns the lenth of string
+ * @num : operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+/**
+ *  *main - prints the first 98 fibonaci sequences
+ *  Return: 0
  */
 
 int main(void)
 {
-	long int fib;
-	long int a = 0;
-	long int b = 1;
-	int counter = 0;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int i = 1, initial0s;
 
-	while (counter < 98)
+	while (i <= 98)
 	{
-		fib = a + b;
-		printf("%li, ", fib);
-		a = b;
-		b = fib;
-		counter++;
-	}
-	fib = a + b;
-	printf("%li\n", fib);
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
 
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+	}
 	return (0);
 }
