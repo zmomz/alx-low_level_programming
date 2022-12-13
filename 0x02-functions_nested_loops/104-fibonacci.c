d513fca026b48056dcd464f897c26fb44999d6d5
zmomz <zmzeromz@gmail.com>
2022-12-13 23:36:50 +0200

    last fibs

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print fibs from 0 to 100
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int fib;
+	long int a = 0;
+	long int b = 1;
+	int counter = 0;
+
+	while (counter < 98)
+	{
+		fib = a + b;
+		printf("%li, ", fib);
+		a = b;
+		b = fib;
+		counter++;
+	}
+	fib = a + b;
+	printf("%li\n", fib);
+
+	return (0);
+}
