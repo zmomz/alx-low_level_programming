2076e9d13f1bb06ff7f397350d01ed87d3f88aa0
zmomz <zmzeromz@gmail.com>
2022-12-13 21:58:37 +0200

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table- print n times table
+ * @n: table dimentions
+ * Retutn: void
+ */
+
+void print_times_table(int n)
+{
+int i, j, x;
+char ender;
+
+if ((n >= 0) & (n <= 15))
+{
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			x = i * j;
+			if (j < n)
+				ender = ',';
+			else
+				ender = '\n';
+			if (j == 0)
+				printf("%i%c", 0, ender);
+			else if (x <= 9)
+				printf("   %i%c", x, ender);
+			else if (x <= 99)
+				printf("  %i%c", x, ender);
+			else
+				printf(" %i%c", x, ender);
+		}
+	}
+}
+else
+	printf("\n");
+}
