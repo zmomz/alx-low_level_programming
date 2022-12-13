875b048857c6ba49d98c199dd1f5a14be8248153
zmomz <zmzeromz@gmail.com>
2022-12-13 18:54:05 +0200

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - print times table from 0 to 9
+ */
+void times_table(void)
+{
+int i, j, n;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		n = i * j;
+		if (n <= 9)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar((n % 10) + '0');
+			if (j != 9)
+				_putchar(',');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			if (j != 9)
+				_putchar(',');
+		}
+	if (j == 9)
+		_putchar('\n');
+	}
+}
+}
