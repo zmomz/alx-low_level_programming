cba4a190c59318a6313d1dd3680ae3bcd1859680
zmomz <zmzeromz@gmail.com>
2022-12-15 12:28:34 +0200

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply a & p
+ * @a: first input
+ * @b: second input
+ * Return: product of a x b.
+ */
+int mul(int a, int b)
+{
+int product = a * b;
+	return (product);
+}
