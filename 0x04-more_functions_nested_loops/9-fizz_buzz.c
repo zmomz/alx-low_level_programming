5cc3278cbadcb61f445f3d18a39eef06c299a48f
zmomz <zmzeromz@gmail.com>
2022-12-15 14:32:25 +0200

    corrected spaces and endline

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 /**
- * fizzbuzz - fizz buzz fizzbuzz stupid task
+ * main - fizz buzz fizzbuzz stupid task
  * Return: 0 always
  */
 
-int fizzbuzz(void)
+int main(void)
 {
 	int i;
+	char space;
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i != 100)
+			space = ' ';
+		else
+			space = '\n';
+
 		if ((i % 15) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz%c", space);
 
 		else if ((i % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz%c", space);
 
 		else if ((i % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz%c", space);
 
 		else
-			printf("%i ", i);
+			printf("%i%c", i, space);
 	}
-	putchar('\n');
 
 	return (0);
 }
