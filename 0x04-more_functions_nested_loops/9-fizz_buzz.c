e06eba904fb13454de99feaaee0caa3add90de16
zmomz <zmzeromz@gmail.com>
2022-12-15 14:15:37 +0200

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - fizz buzz fizzbuzz stupid task
+ * no return
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+
+		else
+			printf("%i ", i);
+	}
+	putchar('\n');
+}
