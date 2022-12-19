61dcb65c438b82474c125f5ce3e9cad58ba8e544
zmomz <zmzeromz@gmail.com>
2022-12-19 19:18:02 +0200

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of s
+ * @s: string input to be valuated
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (; *s++;)
+		len++;
+
+	return len;
+}
